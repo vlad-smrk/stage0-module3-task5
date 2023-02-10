f65b006a205334e9ff8a8c97865a5308c7191e03
Vladislav Samorukov <smrk.vld@gmail.com>
2023-02-10 19:04:05 +0300

    done

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float result = (float)toBeDivided / 5;
+        System.out.println(result);
     }
 }
